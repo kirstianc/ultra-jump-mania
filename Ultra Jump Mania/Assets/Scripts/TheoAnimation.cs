7c2b8abd966729308b6d6e5add3ad2215dd9f0eb
kirstianc <48222417+kirstianc@users.noreply.github.com>
2022-10-04 13:20:27 -0700

    Update TheoAnimation.cs
    -Set idle animation after 5s
    -Added conditions for left/right, down, space

--- Ultra Jump Mania/Assets/Scripts/TheoAnimation.cs
+++ Ultra Jump Mania/Assets/Scripts/TheoAnimation.cs
@@ -5,28 +5,34 @@ using UnityEngine;
 public class TheoAnimation : MonoBehaviour
 {
 
-    private Animator animator;
+    private Animator th_animator;
 
     // Start is called before the first frame update
     void Start()
     {
-        animator = GetComponent<Animator>();
-
-        //StartCoroutine(DieCoroutine());
+        th_animator = GetComponent<Animator>();
+        
+        Enumerator waitFiveSeconds()
+        {
+            yield return new WaitForSeconds(5);
+            anim.Play("Idle");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.LeftArrow)||Input.GetKey(KeyCode.RightArrow)){
+            th_animator.ResetTrigger("Idle");
+            th_animator.SetTrigger("Walk");
+        }
+        if(Input.GetKey(KeyCode.DownArrow)){
+            th_animator.ResetTrigger("Idle");
+            th_animator.SetTrigger("Crouch");
+        }
+        if(Input.GetKey(KeyCode.Space)){
+            th_animator.ResetTrigger("Idle");
+            th_animator.SetTrigger("Jump");
+        }
     }
-
-    
-    IEnumerator DieCoroutine()
-    {
-        yield return new WaitForSeconds(3);
-        animator.SetTrigger("Die");
-        Destroy(gameObject, 1.5f);
-    }
-    
 }
